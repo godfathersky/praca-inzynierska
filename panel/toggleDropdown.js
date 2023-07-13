f8138917d4b1b8fe3f13159d765863b4ec56b2b7
Robert <zimarobert0@gmail.com>
2023-07-13 16:55:46 +0200

    Changed the layout of the 'main' container, added a test animation (css and js) of the side menu return.

--- panel/toggleDropdown.js
+++ panel/toggleDropdown.js
@@ -1,11 +1,29 @@
 function toggleDropdown(){
     var dropdownContent = document.getElementById('dropdownOptContent');
         dropdownContent.classList.toggle('active');
-        dropdownContent.style.transitionDuration=2000;
 }
 
 function toggleSideMenu() {
-    var dropdownContent = document.getElementById("dropdownOptContent");
-    dropdownContent.classList.toggle("open");
-  }
-  
+    var optBtns = document.querySelector(".opt-btns");
+    optBtns.classList.toggle("open");
+
+
+    const key = "isThisSideMenuOpen";
+    const keyValue = true;
+    sessionStorage.setItem(key,keyValue);
+    var checkItem = sessionStorage.getItem(key);
+    var test = document.getElementById("test");
+    if(checkItem){
+        test.style.backgroundColor="red";
+        sessionStorage.removeItem(key);
+    }
+
+    // Dobudować sprawdzanie czy przycisk został naciśnięty
+    // Na podstawie tego dokonywać zmiany ikony
+    // Warto również spojrzeć na obsługę animacji w tym miejscu
+
+    // var checkItem2 = sessionStorage.getItem(key);
+    // if(!checkItem2){
+    //     test.style.backgroundColor="green";
+    // }
+}  

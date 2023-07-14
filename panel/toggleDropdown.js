905ffc71fcf4d614c6ca9bdef2a995a420e97681
Robert <zimarobert0@gmail.com>
2023-07-14 15:29:50 +0200

    Fixed the operation of the animation and added a return animation. Removed unnecessary id and comment fields.

--- panel/toggleDropdown.js
+++ panel/toggleDropdown.js
@@ -1,3 +1,5 @@
+var clickCount = 0;
+
 function toggleDropdown(){
     var dropdownContent = document.getElementById('dropdownOptContent');
         dropdownContent.classList.toggle('active');
@@ -5,25 +7,19 @@ function toggleDropdown(){
 
 function toggleSideMenu() {
     var optBtns = document.querySelector(".opt-btns");
-    optBtns.classList.toggle("open");
+    var icon = document.getElementById("arrowIcon");
 
-
-    const key = "isThisSideMenuOpen";
-    const keyValue = true;
-    sessionStorage.setItem(key,keyValue);
-    var checkItem = sessionStorage.getItem(key);
-    var test = document.getElementById("test");
-    if(checkItem){
-        test.style.backgroundColor="red";
-        sessionStorage.removeItem(key);
+    clickCount++;
+    if (clickCount % 2 === 1){
+        icon.classList.add('fa-circle-arrow-left');
+        icon.classList.remove('fa-circle-arrow-right');
+        optBtns.classList.remove("close-animation");
+        optBtns.classList.add("open-animation");
     }
-
-    // Dobudować sprawdzanie czy przycisk został naciśnięty
-    // Na podstawie tego dokonywać zmiany ikony
-    // Warto również spojrzeć na obsługę animacji w tym miejscu
-
-    // var checkItem2 = sessionStorage.getItem(key);
-    // if(!checkItem2){
-    //     test.style.backgroundColor="green";
-    // }
-}  
+    else{
+        icon.classList.add('fa-circle-arrow-right');
+        icon.classList.remove('fa-circle-arrow-left');
+        optBtns.classList.remove("open-animation");
+        optBtns.classList.add("close-animation");
+    }
+}

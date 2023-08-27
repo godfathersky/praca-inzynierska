2223727ce56793f8be6bc8b10748122ca15ff843
Robert <zimarobert0@gmail.com>
2023-08-27 21:06:52 +0200

    Adding support for the fetch() method to implement registration inside the application.

--- home/regFormCreate.js
+++ home/regFormCreate.js
@@ -248,6 +248,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
         regBtn.addEventListener("click", event =>{
             event.preventDefault();
+
+            let username = document.getElementById("usernameRegInput").value;
+            let email = document.getElementById("emailRegInput").value;
+            let password = document.getElementById("passwordRegInput").value;
+            
+            console.log(`Sending data: ${username}, ${email}, ${password}`);
+        
+            registerUser(username, email, password).then((success) => {
+            if (success) {
+                // Kontynuuj logikę po pomyślnej rejestracji
+                alert("Rejestracja pomyślna!");
+            } else {
+                // Kontynuuj logikę po nieudanej rejestracji
+                alert("Rejestracja nieudana!");
+            }
+            });
+
             resetRegisterInputs();
         });
 
@@ -255,6 +272,42 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             resetRegisterInputs();
         });
+
     }
     createRegContainer();
+
+    async function registerUser(username, email, password) {
+        try {
+          const response = await fetch('https://localhost:7121/api/Users/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              Username: username,
+              Email: email,
+              Password: password
+            }),
+          });
+      
+          let data;
+          const contentType = response.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            data = await response.json();
+          } else {
+            data = await response.text();
+          }
+      
+          if (response.ok) {
+            console.log(data);
+            return true;
+          } else {
+            console.error(data);
+            return false;
+          }
+        } catch (error) {
+          console.error('There was a problem with the fetch operation: ', error);
+          return false;
+        }
+      };
 });

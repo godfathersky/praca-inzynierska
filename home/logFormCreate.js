c026a2c56a9ee103ce645017193fab854a696d7a
Robert <zimarobert0@gmail.com>
2023-08-28 12:37:52 +0200

    Added support for an API endpoint for the login form.

--- home/logFormCreate.js
+++ home/logFormCreate.js
@@ -151,6 +151,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
         logBtn.addEventListener("click", event =>{
             event.preventDefault();
+
+            let username = document.getElementById("usernameLogInput").value;
+            let password = document.getElementById("passwordLogInput").value;
+            
+            console.log(`Sending data: ${username}, ${password}`);
+        
+            loginUser(username, password).then((success) => {
+            if (success) {
+                alert("Logowanie pomyślne!");
+            } else {
+                alert("Logowanie nieudane!");
+            }
+            });
+
             resetLoginInputs();
         });
 
@@ -161,4 +175,38 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     createLogContainer();
+
+    async function loginUser(username, password) {
+        try {
+          const response = await fetch('https://localhost:7121/api/Users/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              Username: username,
+              Password: password
+            }),
+          });
+      
+          let data;
+          const contentType = response.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            data = await response.json();
+          } else {
+            data = await response.text();
+          }
+      
+          if (response.ok) {
+            console.log(data);
+            return true;
+          } else {
+            console.error(data);
+            return false;
+          }
+        } catch (error) {
+          console.error('There was a problem with the fetch operation: ', error);
+          return false;
+        }
+      };
 });

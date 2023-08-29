6c5c62670768365d5b13fc9d44bcb6f722599a29
Robert <zimarobert0@gmail.com>
2023-08-29 14:58:08 +0200

    Added basic pageloader functionality for the application.

--- panel/handlingAnimation.js
+++ panel/handlingAnimation.js
@@ -23,3 +23,17 @@ function toggleSideMenu() {
         optBtns.classList.add("close-animation");
     }
 }
+
+window.addEventListener('load', function() {
+    this.setTimeout(function() {
+        const pageloader = document.getElementById('pageloader');
+        pageloader.style.display = 'none';
+      
+        const nav = document.getElementsByTagName('nav')[0];
+        const header = document.getElementsByTagName('header')[0];
+        const main = document.getElementsByTagName('main')[0];
+        nav.style.display = 'flex';
+        header.style.display = 'flex';
+        main.style.display = 'flex';
+    }, 5000);
+  });
